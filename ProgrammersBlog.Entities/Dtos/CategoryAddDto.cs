005a36bcd5efb260f85b5434b7fc70786c0d2ad7
Arda <76245133+Ardakaracorlu@users.noreply.github.com>
2022-02-02 08:02:08 +0300

    CategoryPartial

--- ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
+++ ProgrammersBlog.Entities/Dtos/CategoryAddDto.cs
@@ -11,24 +11,21 @@ namespace ProgrammersBlog.Entities.Dtos
     public class CategoryAddDto
     {
         [DisplayName("Kategori Adı")]
-        [Required(ErrorMessage ="{0} boş geçilmemelidir. ")] // buradaki {0} Dinamiktir İçine Kategori Adı gelir, 1 ise uzunluğu gelir.
-        [MaxLength(70,ErrorMessage ="{0,{1} Karakterden büyük olmamalıdır.")]
-        [MinLength(3,ErrorMessage ="{0,{1} Karakterden az olmamalıdır. !")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Name { get; set; }
-
-        [DisplayName("Kategori Açıklaması")] 
-        [MaxLength(500, ErrorMessage = "{0,{1} Karakterden büyük olmamalıdır !")]
-        [MinLength(3, ErrorMessage = "{0,{1} Karakterden az olmamalıdır !")]
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Description { get; set; }
-
         [DisplayName("Kategori Özel Not Alanı")]
-        [MaxLength(70, ErrorMessage = "{0,{1} Karakterden büyük olmamalıdır !")]
-        [MinLength(3, ErrorMessage = "{0,{1} Karakterden az olmamalıdır !")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
         public string Note { get; set; }
-
         [DisplayName("Aktif Mi?")]
-        [Required(ErrorMessage = "{0} boş geçilmemelidir. !")]
-        public bool IsActıve { get; set; }
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        public bool IsActive { get; set; }
 
     }
 }
